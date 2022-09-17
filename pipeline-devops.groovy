3861ea0d2b75d19e02714bd53ddd630a60db52e2
danieltinetum <daniel.trejo@inetum.com>
2022-09-17 01:11:25 -0500

    1

--- pipeline-devops.groovy
+++ pipeline-devops.groovy
@@ -1,14 +1,13 @@
 @Library('devops-selenium-functional-tests@develop')
 
-node {
-
-    stage('Preparation') { // for display purposes
-        sh 'echo Preparation from pipeline file' 
+    node{
+        stage("Preparation") {
+          sh 'echo Preparation'
+        }
+        stage("Starting Test") {
+            sh 'echo Test'
+        }
+        stage("Sending Report") {
+             sh 'echo Report'
+        }
     }
-    stage('Build') {
-        sh 'echo Build from pipeline file' 
-    }
-    stage('Results') {
-         sh 'echo Results from pipeline file' 
-    }
-}
